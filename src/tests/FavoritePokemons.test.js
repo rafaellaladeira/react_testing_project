c43833c2284cb6cbbdf04e511aab34b788c1cf11
Rafaella Ladeira <rafaella.ladeira@gmail.com>
2022-03-23 11:18:24 -0300

    Requisito 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from './renderWithRouter';
+import FavoritePokemons from '../components/FavoritePokemons';
+import App from '../App';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  test('Se é exibido No favorite pokemon found se não tiver pokémons fav', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const text = screen.getByText(/No favorite pokemon found/i);
+    expect(text).toBeInTheDocument();
+  });
+
+  test('Se é exibido todos os cards de pokémons favoritados', () => {
+    const { history } = renderWithRouter(<App />);
+    const { pathname } = history.location;
+    const textDetails = screen.getByRole('link', { name: /More details/i });
+    expect(pathname).toBe('/');
+    userEvent.click(textDetails);
+
+    const favoritePokemonText = screen.getByText(/Pokémon favoritado/i);
+    expect(history.location.pathname).toBe('/pokemons/25'); // ____________-
+    userEvent.click(favoritePokemonText);
+
+    const favoritePokemonsPage = screen.getByRole('link', { name: /Favorite Pokémons/i });
+    expect(history.location.pathname).toBe('/pokemons/25');
+    userEvent.click(favoritePokemonsPage);
+
+    const pikachu = screen.getByText(/pikachu/i);
+    expect(history.location.pathname).toBe('/favorites');
+    expect(pikachu).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <NotFound.js />', () => {
+  test('Se página contém um heading h2 com o texto Page requested not found', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByText(/Page requested not found/i);
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif', () => {
+    renderWithRouter(<NotFound />);
+    const altImg = screen
+      .getByAltText('Pikachu crying because the page requested was not found');
+    expect(altImg).toBeInTheDocument();
+    expect(altImg.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
